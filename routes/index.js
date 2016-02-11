401d60986d35e6086da12874d9d1290208bc0a16
Divya <divyaraval25@outlook.com>
2016-02-10 18:40:30 -0800

    6 link changes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express on Azure' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express on Azure' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Express on Azure' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Express on Azure' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express on Azure' });
+});
+
 module.exports = router;
